a390a656566099bee9a54cb06fafeb42eb07465e
Jonny Lai <jonny.lai@latticeworkinc.com>
2024-05-09 18:41:18 +0800

    [Font] Set guifont to "Source Code Pro".

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Themes ---------------------------------------------------------------------
+
+if vim.fn.has("gui_running") then
+  vim.opt.guifont = { "Source Code Pro:h14" }
+end
