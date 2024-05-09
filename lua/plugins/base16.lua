b1044953bd4728bc6ec3afe96205e8f05290c6ad
Chu-Siang Lai <chusiang.lai@gmail.com>
2024-05-09 23:51:20 +0800

    [base16] Update comment.

--- lua/plugins/base16.lua
+++ lua/plugins/base16.lua
@@ -1,9 +1,4 @@
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
+-- base16.lua for base16-vim (https://github.com/chriskempson/base16-vim) .
 
 return {
   -- add base16-vim
