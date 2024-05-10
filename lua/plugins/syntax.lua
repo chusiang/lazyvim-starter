17f2c17a3de94b002abf595192c2a06e01579979
Jonny Lai <jonny.lai@latticeworkinc.com>
2024-05-10 16:14:48 +0800

    [syntax] Add  Jinja2, HAProxy, Nginx, Dockerfile, Ansible, Textile

---
+++ lua/plugins/syntax.lua
@@ -0,0 +1,10 @@
+-- syntax.lua
+
+return {
+  { "Glench/Vim-Jinja2-Syntax" },
+  { "Joorem/vim-haproxy" },
+  { "chr4/nginx.vim" },
+  { "ekalinin/Dockerfile.vim" },
+  { "pearofducks/ansible-vim" },
+  { "timcharper/textile.vim" },
+}
