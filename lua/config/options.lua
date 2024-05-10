b4f1bf2954f5598191f3a70c9b0cc610d671db01
Jonny Lai <jonny.lai@latticeworkinc.com>
2024-05-10 16:26:57 +0800

    [option] Enable autowrapping.
    > https://github.com/LazyVim/LazyVim/discussions/1959#discussioncomment-7494729

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,14 @@
+--  LazyVim -------------------------------------------------------------------
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
 
 -- Themes ---------------------------------------------------------------------
 
 -- SauceCodePro Nerd Font: Fork from "Source Code Pro" font.
 --
 -- > https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/SourceCodePro.zip
-vim.opt.guifont = { "SauceCodePro Nerd Font:h20" }
+vim.opt.guifont = { "SauceCodePro Nerd Font:h14" }
+
+-- Auto line break.
+vim.opt.wrap = true
